a4e39c5bd282abd69be2447d7a0837c88f80c960
Teja-thota <tejathota2718@gmail.com>
2025-07-27 11:45:20 +0530

    Add project files.

---
+++ CosoleJenk/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CosoleJenk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my name is teja thota");
+            Console.WriteLine("my age is 21");
+            Console.WriteLine("my city is giddalur");
+            Console.WriteLine("pincode:523357");
+        }
+    }
+}
